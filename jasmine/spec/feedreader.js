97ef78d355afe2ee537cf7988c68cec0a24820b7
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-06-20 13:23:10 -0700

    Test for hidden menu on load

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,15 +59,18 @@ $(function() {
 
     });
 
-
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('has a hidden menu by default', function() {
+            //body class menu-hidden is removed to show menu
+            var body = document.getElementsByTagName('body')[0];
+            expect(body.className).toEqual('menu-hidden');
+        });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
@@ -89,4 +92,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
