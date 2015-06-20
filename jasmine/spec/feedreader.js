6e420478e6155f2413f788ec2bcf966e28397e66
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-06-20 13:35:13 -0700

    refactor 'The Menu' suite, add beforeEach and test for menu toggle

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,6 +61,12 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
+
+        var body = null;
+        beforeEach(function() {
+            body = document.getElementsByTagName('body')[0];
+        });
+
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -68,14 +74,21 @@ $(function() {
          */
         it('has a hidden menu by default', function() {
             //body class menu-hidden is removed to show menu
-            var body = document.getElementsByTagName('body')[0];
             expect(body.className).toEqual('menu-hidden');
         });
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+         it('toggles the menu visibility when clicked', function() {
+            $('.menu-icon-link').click();
+            expect(body.className).toEqual('');
+            $('.menu-icon-link').click();
+            expect(body.className).toEqual('menu-hidden');
+         });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
@@ -92,5 +105,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });
+
 }());
